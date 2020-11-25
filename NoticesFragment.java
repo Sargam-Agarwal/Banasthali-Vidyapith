fe78032af5d238b91716f31f61fb5895e3fc5578
Sargam Agarwal <72149858+Sargam-Agarwal@users.noreply.github.com>
2020-11-24 23:31:30 -0800

    Add files via upload

---
+++ NoticesFragment.java
@@ -0,0 +1,36 @@
+package com.sargam.banasthalicollege.banasthalividyapith;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+
+public class NoticesFragment extends Fragment{
+
+    RecyclerView mNoticeRecyclerView;
+
+
+
+
+    public NoticesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_notices, container, false);
+        return view;
+    }
+}
